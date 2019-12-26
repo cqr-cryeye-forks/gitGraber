1dd358a86b282d40d22c0f3b9e24389d5ff4e7a6
natrix-fork <cqrdev@gmail.com>
2019-12-26 15:49:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-GITHUB_TOKENS = ['X', 'X', 'X', 'X', 'X']
+GITHUB_TOKENS = ['9ae274928d516eb52322730d314832459e0c48a8',
+                 '2fb854bd35d6df72209209c460104054b55046c9',
+                 '328b95ba488a5506791dc79e8b87c81511aaaddf',
+                 'ea573421400540158efb2b749d93d32abe21d2e0',
+                 '4a4db085e8812a5f60bffae7b9c6745112e67d50']
 GITHUB_URL_FILE = 'rawGitUrls.txt'
 GITHUB_API_URL = 'https://api.github.com/search/code?q='
 GITHUB_API_COMMIT_URL = 'https://api.github.com/repos/'
